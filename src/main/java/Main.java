8d9299c9c19a86e59d45b5a92d9393b9bb6d6258
Andrej_mit_j <andrejart@gmx.de>
2023-01-10 14:46:29 +0100

    utility package

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import utility.Scraper;
+
 import java.util.Scanner;
 
 /**
---
+++ src/main/java/utility/XMLParser.java
@@ -0,0 +1,13 @@
+package utility;
+
+import java.util.Map;
+
+/**
+ * Class to analyze the XML files and create objects
+ * @author Andrej Artuschenko
+ */
+public class XMLParser {
+
+
+
+}
