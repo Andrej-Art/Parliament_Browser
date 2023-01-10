41b4ab353edc6558cb082c79b209fdd933667ade
Eric_Lakhter <eric.lakhter@gmx.de>
2023-01-10 18:55:46 +0100

    Better JavaDoc for @Testing

--- src/main/java/utility/Testing.java
+++ src/main/java/utility/Testing.java
@@ -3,6 +3,15 @@ package utility;
 /**
  * This annotation marks a class or method which is solely used for testing,
  * going unused in the finished product.
+ * <p>Example:
+ * <pre>
+ * <span style="color:lime">/**
+ *  * This is to test X.
+ *  *&#47;</span>
+ * <span style="color:yellow">@Testing</span>
+ * public static void main(String[] args) {
+ *     // ... code goes here
+ * }</pre>
  * @author Eric Lakhter
  */
 public @interface Testing {
