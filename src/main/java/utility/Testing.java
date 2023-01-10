1e2f3d89c78087805bed98f8df3d7c86017fcd44
Eric_Lakhter <eric.lakhter@gmx.de>
2023-01-10 16:41:43 +0100

    Inserted the NLP_Example.html into newly created UIMAResources.java Added Testing annotation for convenience Shared /.idea/ folder

---
+++ src/main/java/utility/Testing.java
@@ -0,0 +1,9 @@
+package utility;
+
+/**
+ * This annotation marks a class or method which is solely used for testing,
+ * going unused in the finished product.
+ * @author Eric Lakhter
+ */
+public @interface Testing {
+}
