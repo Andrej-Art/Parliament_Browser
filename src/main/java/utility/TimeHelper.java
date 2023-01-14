d7e80810934d364fea9f8aa1ed1a2154b0b05eb4
Eric_Lakhter <eric.lakhter@gmx.de>
2023-01-14 23:47:04 +0100

    Added author to TimeHelper

--- src/main/java/utility/TimeHelper.java
+++ src/main/java/utility/TimeHelper.java
@@ -20,6 +20,7 @@ public abstract class TimeHelper {
      * Converts dates of the pattern {@code dd.MM.yyyy} to the ISO-8601 calendar system.
      * @param date Date String.
      * @return Respective {@code LocalDate}.
+     * @author Eric Lakhter
      */
     public static LocalDate convertToISOdate(String date){
         return LocalDate.parse(date, DATE_FORMAT_INPUT);
@@ -29,6 +30,7 @@ public abstract class TimeHelper {
      * Converts times of the pattern {@code H:mm} to the ISO-8601 calendar system.
      * @param time Time String.
      * @return Respective {@code LocalTime}.
+     * @author Eric Lakhter
      */
     public static LocalTime convertToISOtime(String time){
         return LocalTime.parse(time.replace(".", ":").replace(" Uhr", ""), CLOCK_FORMAT_INPUT);
@@ -41,6 +43,7 @@ public abstract class TimeHelper {
      * @param begin Start time.
      * @param end End time.
      * @return The time between {@code begin} and {@code end} in minutes.
+     * @author Eric Lakhter
      */
     public static long durationBetweenTimes(LocalTime begin, LocalTime end) {
         long dauer = ChronoUnit.MINUTES.between(begin, end);
