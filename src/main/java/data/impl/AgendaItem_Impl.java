e6a54cfbca953b77254f51c18880f2e9ac904441
s2008331 <s2008331@stud.uni-frankfurt.de>
2023-01-29 19:06:43 +0100

    Rückbau der Datenstruktur

--- src/main/java/data/impl/AgendaItem_Impl.java
+++ src/main/java/data/impl/AgendaItem_Impl.java
@@ -48,29 +48,6 @@ public class AgendaItem_Impl implements AgendaItem {
         this.speechIDs = (ArrayList<String>) document.get("speechIDs");
     }
 
-    /**
-     * Full {@code AgendaItem} constructor which fills the class variables from parameters provided.
-     * @param id  The AgendaItem's unique  id made up of the concatenation of  ProtocolId + "/" + AgendaItemID
-     * @param subject The subject-matter of the Item
-     * @author DavidJordan
-     */
-    public AgendaItem_Impl(String id, String subject) {
-        this._id = id;
-        this.subject = subject;
-    }
-
-    /**
-     * This implements the addition of Data after parsing the data of the table oof contents
-     * @param date
-     * @param speechIDs
-     * @author Julian Ocker
-     */
-    public void setDateAndIDs(LocalDate date, ArrayList<String> speechIDs){
-        this.date = date;
-        this.speechIDs = speechIDs;
-    }
-
-
     @Override
     public String getID() {
         return this._id;
